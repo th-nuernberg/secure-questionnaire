9d1781dff81a98995342f9b4d79d867a5717bb4c
m-luiisa <44697160+m-luiisa@users.noreply.github.com>
2022-05-25 12:46:20 +0200

    manuelle Codeeingabe für AnswersEdit entfernt

--- flaskapp/frontend/src/components/util/CodeManually.vue
+++ flaskapp/frontend/src/components/util/CodeManually.vue
@@ -1,7 +1,6 @@
 <template>
     <b-container class="container">
         <b-row>
-            <b-col>
                 <h5>Neuen Fragebogen ausfüllen</h5>
                 <b-form-input class="input-field mt-2"
                               v-model="codeNew"
@@ -9,17 +8,6 @@
                               :state="codeValidation"
                               @input="codeValidation = null">
                 </b-form-input>
-            </b-col>
-
-            <b-col>
-                <h5>Antworten bearbeiten</h5>
-                <b-form-input class="input-field mt-2"
-                              v-model="codeEdit"
-                              placeholder="Code"
-                              :state="codeValidation"
-                              @input="codeValidation = null">
-                </b-form-input>
-            </b-col>
         </b-row>
         <b-button class="mt-3" variant="primary" @click="submit()">Bestätigen</b-button>
     </b-container>
@@ -30,27 +18,14 @@
         data() {
             return {
                 codeNew: "",
-                codeEdit: "",
                 codeValidation: null,
             };
         },
 
         methods: {
             submit() {
-                if (this.codeNew !== "" && this.codeEdit !== "") {
-                    this.codeValidation = false;
-                }
-                else if (this.codeNew !== "") {
+                if (this.codeNew !== "") {
                     this.$router.push({path: `/patient/questionnaire/${this.codeNew}` });
-                }
-                else if (this.codeEdit !== "") {
-                    this.$store.dispatch("getAnswers", this.codeEdit)
-                        .then((queID) => {
-                            this.$router.push({ name: 'PatientQuestionnaire', params: { id: queID } })
-                        })
-                        .catch(() => {
-                        })
-;
                 }
                 else {
                     this.codeValidation = false
