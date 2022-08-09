9e6e3b2c658979adfaa56b60a46ebdda193edca4
erzence81136 <celina.erzen@web.de>
2022-08-09 16:27:54 +0200

    Aufgabe 6 fertig

--- SKT/src/views/task/task6.vue
+++ SKT/src/views/task/task6.vue
@@ -9,7 +9,15 @@
     <h5>Sie sehen hier auf dieser Tafel verschiedene Symbole: Vierecke, Sterne und Blumen. 
       Wichtig sind nur die Vierecke
       Bitte zählen Sie sie so schnell sie können</h5><br>     
-     <Symbols :listed_numbers="selectedNumber"/><br> 
+     <Symbols :listed_numbers="selectedNumber" @anzahl="anzahlSymbole"/><br> 
+
+    <div class="input-group mb-3" style="width: 500px; padding-left: 120px;">
+        <input type="text" class="form-control" placeholder="Anzahl gezählter Vierecke" v-model="eingabe" >
+        <div class="input-group-append">
+          <button class="btn btn-primary" type="text" @click="this.auswertung">OK</button>
+        </div>
+      </div>
+
     <router-link class="btn-router" to="/task7" @click="timeOut">Weiter</router-link><br><br>
     </div>  
 
@@ -40,6 +48,10 @@ export default {
     return{
       selectedNumber: [],
       hide: Boolean,     
+      anzahl: '',
+      eingabe: '',
+      ergebnis: Boolean
+      
     };
   },
   methods: {
@@ -52,7 +64,18 @@ export default {
     },
     finishedTask(){
       this.hide = true;
-      this.$store.dispatch("addData", {'task':4, 'content':{'time':this.$refs.timeBar.time}})
+      this.$store.dispatch("addData", {'task':6,'content':{'time':this.$refs.timeBar.time,'result':this.ergebnis}})
+    },
+    anzahlSymbole(anzahl){
+      this.anzahl = anzahl;
+      console.log(this.anzahl);
+    },
+    auswertung(){
+      if (this.eingabe == this.anzahl){
+        this.ergebnis = true;
+      } else {
+        this.ergebnis = false;
+      }       
     },
   },
   created() {
