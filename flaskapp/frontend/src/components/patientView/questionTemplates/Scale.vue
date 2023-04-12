81647af43286a95a1c1ea7ce3751a4d154ede8fb
schoenwr <96129633+schoenwr@users.noreply.github.com>
2023-04-12 15:05:04 +0200

    fixed Scale.vue

--- flaskapp/frontend/src/components/patientView/questionTemplates/Scale.vue
+++ flaskapp/frontend/src/components/patientView/questionTemplates/Scale.vue
@@ -3,19 +3,11 @@
         <heading :question="question"></heading>
         <p class="mb-0">{{question.description}}</p>
 
-        <form class="px-2" text-align="left" action="">
-            <!-- <b-form-radio-group v-model="answer" @input="input()" :name="name">
-                <b-form-radio v-for="option in question.options" :key="option.id" :value="option.text">
-                    {{option.text}}
-                </b-form-radio>
-            </b-form-radio-group> -->
-            <div class="form-check" v-for="option in question.options" :key="option.id" :value=option.text  >
-                        <input class="form-check-input" type="checkbox" name="exampleForm" id="checkbox_single" />
-                        <label class="form-check-label" margin-left="20px" for="checkbox_single">
-                            {{option.text}}
-                        </label>
-            </div>
-        </form>
+        <div class="form-group form-check" v-for="option in question.options" >
+            <label class="check-label" :for="option.text" >{{option.text}}</label>
+            <input type="checkbox"  v-model="answer"  :key="option.id" :value="option.text" @change="input()" :name="name" :true-value="option.text" />
+        </div>
+        <div>Selected: {{answer}}</div>
     </div>
 </template>
 
