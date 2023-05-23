4fbd6f76de1263f511e0472802dd59a0efa7180f
Chris Stade <christoph-stade@outlook.de>
2023-05-23 19:54:08 +0200

    Remove HelloWorld

--- flaskapp/frontend/src/App.vue
+++ flaskapp/frontend/src/App.vue
@@ -1,24 +1,9 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-//import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
    <div id="app">
          <div>
             <!-- navbar -->
             <NavBar />
         </div>
-          <!--  <div>-->
-          <!--    <a href="https://vitejs.dev" target="_blank">-->
-          <!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
-          <!--    </a>-->
-          <!--    <a href="https://vuejs.org/" target="_blank">-->
-          <!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
-          <!--    </a>-->
-          <!--  </div>-->
-          <!--  <HelloWorld msg="Vite + Vue" />-->
             <router-view></router-view>
           <div>
             <!-- footer -->
