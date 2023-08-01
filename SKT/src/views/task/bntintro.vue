b922c6eca446cb4c5368547272ce4c173e395b96
Sebastian Bayerl <me@sebastianbayerl.de>
2023-08-01 11:59:39 +0200

    increase font size

--- SKT/src/views/task/bntintro.vue
+++ SKT/src/views/task/bntintro.vue
@@ -2,16 +2,14 @@
 
   <body>
     <h3>Bilder benennen</h3>
-    <p>
-      Ihnen werden jetzt nacheinander Bilder von Dingen gezeigt.<br />
-      Ihre Aufgabe ist es, die Gegenstände gut verständlich zu benennen.
-      Nehmen Sie sich dafür ruhig einen Augenblick Zeit.
-      Wenn Sie das Konzept genannt haben, klicken Sie auf "Weiter".
-      Sollten Sie einmal nicht wissen, was das Bild darstellt, klicken Sie bitte ebenfalls auf "Weiter".
-      Die Anzahl korrekt erkannter Bilder wird am Ende des Tests angezeigt.
+ <h4>     Ihnen werden jetzt nacheinander Bilder von Dingen gezeigt.<br />
+      Ihre Aufgabe ist es, die Gegenstände gut verständlich zu benennen.<br />
+      <b>Nehmen Sie sich dafür ruhig einen Augenblick Zeit, sprechen Sie langsam und deutlich.</b><br />
+      Wenn Sie das Konzept genannt haben, klicken Sie auf "Weiter".<br />
+      Sollten Sie einmal nicht wissen, was das Bild darstellt, klicken Sie bitte ebenfalls auf "Weiter".<br />
+      Die Anzahl korrekt erkannter Bilder wird am Ende des Tests angezeigt.</h4>
     
       <br />
-    </p>
     <br />
 
     <router-link class="btn-router" to="/bntperform">Los geht's</router-link>
