c9af9453bd52f7eff32794ad04019f5596fbc26a
tschadapa84654 <tschadapa84654@th-nuernberg.de>
2023-11-07 15:18:27 +0100

    Revised template for number questions (PatientView)

--- flaskapp/frontend/src/components/patientView/questionTemplates/Number.vue
+++ flaskapp/frontend/src/components/patientView/questionTemplates/Number.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
+  <!-- <div>
     <heading :question="question"></heading>
     <input type="number" v-model="answer" @input="input()" />
+  </div> -->
+
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="py-3">
+        <label for="number" class="form-label"><heading :question="question"></heading></label>
+        <input type="number" v-model="answer" id="number" class="form-control ml-0" required @input="input()" />
+      </div>
+    </div>
   </div>
+
+  <hr class="my-3">
 </template>
 
 <script>
